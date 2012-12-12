9ff9e55d480818c12190c7b505f94d4818a5363a
Miekka MacArthur <info@miekkamacarthur.com>
2012-12-12 15:47:05 +0100

    testing removal of opt-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,11 +54,11 @@ class User < ActiveRecord::Base
   private
 
   def add_user_to_mailchimp
-    unless self.email.include?('@example.com') or !self.opt_in?
+    unless self.email.include?('@example.com') 
       mailchimp = Hominid::API.new(ENV["MAILCHIMP_API_KEY"])
-      list_id = mailchimp.find_list_id_by_name "visitors"
+      list = mailchimp.find_by_name "visitors"
       info = { }
-      result = mailchimp.list_subscribe(list_id, self.email, info, 'html', false, true, true, false)
+      result = mailchimp.list_subscribe(list, self.email, info, 'html', false, true, false, true)
       Rails.logger.info("MAILCHIMP SUBSCRIBE: result #{result.inspect} for #{self.email}")
     end
   end
@@ -66,8 +66,8 @@ class User < ActiveRecord::Base
   def remove_user_from_mailchimp
     unless self.email.include?('@example.com')
       mailchimp = Hominid::API.new(ENV["MAILCHIMP_API_KEY"])
-      list_id = mailchimp.find_list_id_by_name "visitors"
-      result = mailchimp.list_unsubscribe(list_id, self.email, true, false, true)  
+      list = mailchimp.find_by_name "visitors"
+      result = mailchimp.list_unsubscribe(list, self.email, true, false, true)  
       Rails.logger.info("MAILCHIMP UNSUBSCRIBE: result #{result.inspect} for #{self.email}")
     end
   end
