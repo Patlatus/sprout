f84bc620a6aa36d5451687566949638992042226
Miekka MacArthur <info@miekkamacarthur.com>
2012-12-12 16:33:00 +0100

    hope it owrks

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,7 +67,7 @@ private
     unless self.email.include?('@example.com')
       mailchimp = Hominid::API.new(ENV["MAILCHIMP_API_KEY"])
       list_id = mailchimp.find_list_id_by_name "visitors"
-      result = mailchimp.list_unsubscribe(list_id, self.email, info, 'html', false, true, false, true)
+      result = mailchimp.list_unsubscribe(list_id, self.email, true, false, true)
       Rails.logger.info("MAILCHIMP UNSUBSCRIBE: result #{result.inspect} for #{self.email}")
     end
   end
